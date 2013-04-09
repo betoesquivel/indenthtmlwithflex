c044ae1b1017ec27eaab1f049169c718c5a6b17f
Jose Esquivel <beto.esquivel.p@gmail.com>
2013-04-09 00:38:33 -0500

    Cuando cierro una tag, imprimo los tabs y entonces no puedo irme un tab para atrás si toca cerrar tag

--- lex.yy.c
+++ lex.yy.c
@@ -377,10 +377,10 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static yyconst flex_int16_t yy_accept[14] =
+static yyconst flex_int16_t yy_accept[16] =
     {   0,
-        0,    0,    0,    0,    9,    7,    6,    3,    4,    1,
-        5,    2,    0
+        0,    0,    0,    0,    0,    0,    9,    7,    6,    3,
+        4,    1,    5,    2,    0
     } ;
 
 static yyconst flex_int32_t yy_ec[256] =
@@ -420,28 +420,28 @@ static yyconst flex_int32_t yy_meta[6] =
         1,    1,    2,    1,    1
     } ;
 
-static yyconst flex_int16_t yy_base[16] =
+static yyconst flex_int16_t yy_base[18] =
     {   0,
-        0,    0,    5,    0,    8,   10,   10,    0,   10,    0,
-       10,   10,   10,    5,    4
+        0,    0,    5,    0,    0,    0,    8,   10,   10,    0,
+       10,    0,   10,   10,   10,    5,    4
     } ;
 
-static yyconst flex_int16_t yy_def[16] =
+static yyconst flex_int16_t yy_def[18] =
     {   0,
-       13,    1,    1,    3,   13,   13,   13,   14,   13,   15,
-       13,   13,    0,   13,   13
+       15,    1,    1,    3,    1,    1,   15,   15,   15,   16,
+       15,   17,   15,   15,    0,   15,   15
     } ;
 
 static yyconst flex_int16_t yy_nxt[16] =
     {   0,
-        6,    7,    6,    8,    9,   12,   11,   13,   10,    5,
-       13,   13,   13,   13,   13
+        8,    9,    8,   10,   11,   14,   13,   15,   12,    7,
+       15,   15,   15,   15,   15
     } ;
 
 static yyconst flex_int16_t yy_chk[16] =
     {   0,
-        1,    1,    1,    1,    1,   15,   14,    5,    3,   13,
-       13,   13,   13,   13,   13
+        1,    1,    1,    1,    1,   17,   16,    7,    3,   15,
+       15,   15,   15,   15,   15
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -461,10 +461,12 @@ char *yytext;
 #line 1 "htmlFormat2.l"
 int indents = 0;
  
-#line 465 "lex.yy.c"
+
+#line 466 "lex.yy.c"
 
 #define INITIAL 0
 #define textbefore 1
+#define indento 2
 
 #ifndef YY_NO_UNISTD_H
 /* Special case for "unistd.h", since it is non-ANSI. We include it way
@@ -649,9 +651,9 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
     
-#line 3 "htmlFormat2.l"
+#line 4 "htmlFormat2.l"
 
-#line 655 "lex.yy.c"
+#line 657 "lex.yy.c"
 
 	if ( !(yy_init) )
 		{
@@ -704,7 +706,7 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 14 )
+				if ( yy_current_state >= 16 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -736,48 +738,49 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 4 "htmlFormat2.l"
+#line 5 "htmlFormat2.l"
 endline(); imprimirIndent(); imprimir(); indents++; BEGIN 0; 
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 5 "htmlFormat2.l"
+#line 6 "htmlFormat2.l"
 endline(); indents--; imprimirIndent(); imprimir(); BEGIN 0; 
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 6 "htmlFormat2.l"
-imprimir(); indents++;  
+#line 7 "htmlFormat2.l"
+imprimirIndent(); imprimir(); indents++;  
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 7 "htmlFormat2.l"
-imprimir(); endline(); imprimirIndent(); BEGIN 0; 
+#line 8 "htmlFormat2.l"
+imprimir(); endline(); imprimirIndent(); BEGIN 0;
 	YY_BREAK
 case 5:
 YY_RULE_SETUP
-#line 8 "htmlFormat2.l"
+#line 9 "htmlFormat2.l"
 indents--; imprimirIndent(); imprimir(); 
 	YY_BREAK
 case 6:
 /* rule 6 can match eol */
 YY_RULE_SETUP
-#line 9 "htmlFormat2.l"
+#line 10 "htmlFormat2.l"
 
 	YY_BREAK
 case 7:
 YY_RULE_SETUP
-#line 10 "htmlFormat2.l"
+#line 11 "htmlFormat2.l"
 imprimir(); BEGIN textbefore; 
 	YY_BREAK
 case 8:
 YY_RULE_SETUP
-#line 11 "htmlFormat2.l"
+#line 12 "htmlFormat2.l"
 ECHO;
 	YY_BREAK
-#line 779 "lex.yy.c"
+#line 781 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 case YY_STATE_EOF(textbefore):
+case YY_STATE_EOF(indento):
 	yyterminate();
 
 	case YY_END_OF_BUFFER:
@@ -1068,7 +1071,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 14 )
+			if ( yy_current_state >= 16 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -1096,11 +1099,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 14 )
+		if ( yy_current_state >= 16 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 13);
+	yy_is_jam = (yy_current_state == 15);
 
 	return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1774,7 +1777,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 11 "htmlFormat2.l"
+#line 12 "htmlFormat2.l"
 
 
     void endline(){
