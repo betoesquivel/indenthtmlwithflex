73b73d6f58b08ed361580e57c8ba06814b29b1cc
Jose Esquivel <beto.esquivel.p@gmail.com>
2013-04-09 00:52:00 -0500

    No funciona la regla que busca que enfrente haya una combinación cualquiera de espacios en blanco seguida de portería

--- lex.yy.c
+++ lex.yy.c
@@ -368,8 +368,8 @@ static void yy_fatal_error (yyconst char msg[]  );
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
 
-#define YY_NUM_RULES 8
-#define YY_END_OF_BUFFER 9
+#define YY_NUM_RULES 9
+#define YY_END_OF_BUFFER 10
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -377,10 +377,10 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static yyconst flex_int16_t yy_accept[16] =
+static yyconst flex_int16_t yy_accept[19] =
     {   0,
-        0,    0,    0,    0,    0,    0,    9,    7,    6,    3,
-        4,    1,    5,    2,    0
+        0,    0,    0,    0,    0,    0,   10,    8,    7,    3,
+        4,    1,    6,    0,    0,    2,    5,    0
     } ;
 
 static yyconst flex_int32_t yy_ec[256] =
@@ -388,10 +388,10 @@ static yyconst flex_int32_t yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    3,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    4,
-        1,    5,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    3,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    4,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    5,
+        1,    6,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -415,33 +415,37 @@ static yyconst flex_int32_t yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst flex_int32_t yy_meta[6] =
+static yyconst flex_int32_t yy_meta[7] =
     {   0,
-        1,    1,    2,    1,    1
+        1,    1,    1,    2,    1,    1
     } ;
 
-static yyconst flex_int16_t yy_base[18] =
+static yyconst flex_int16_t yy_base[22] =
     {   0,
-        0,    0,    5,    0,    0,    0,    8,   10,   10,    0,
-       10,    0,   10,   10,   10,    5,    4
+        0,    0,    9,    8,    0,    0,   12,   15,   15,    0,
+        4,    0,   15,    0,    0,   15,   15,   15,    9,    8,
+        6
     } ;
 
-static yyconst flex_int16_t yy_def[18] =
+static yyconst flex_int16_t yy_def[22] =
     {   0,
-       15,    1,    1,    3,    1,    1,   15,   15,   15,   16,
-       15,   17,   15,   15,    0,   15,   15
+       18,    1,    1,    1,    1,    1,   18,   18,   18,   19,
+       18,   20,   18,   11,   21,   18,   18,    0,   18,   18,
+       18
     } ;
 
-static yyconst flex_int16_t yy_nxt[16] =
+static yyconst flex_int16_t yy_nxt[22] =
     {   0,
-        8,    9,    8,   10,   11,   14,   13,   15,   12,    7,
-       15,   15,   15,   15,   15
+        8,    9,    8,    8,   10,   11,   14,   17,   15,   16,
+       13,   18,   12,   12,    7,   18,   18,   18,   18,   18,
+       18
     } ;
 
-static yyconst flex_int16_t yy_chk[16] =
+static yyconst flex_int16_t yy_chk[22] =
     {   0,
-        1,    1,    1,    1,    1,   17,   16,    7,    3,   15,
-       15,   15,   15,   15,   15
+        1,    1,    1,    1,    1,    1,   11,   21,   11,   20,
+       19,    7,    4,    3,   18,   18,   18,   18,   18,   18,
+       18
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -462,7 +466,7 @@ char *yytext;
 int indents = 0;
  
 
-#line 466 "lex.yy.c"
+#line 470 "lex.yy.c"
 
 #define INITIAL 0
 #define textbefore 1
@@ -653,7 +657,7 @@ YY_DECL
     
 #line 4 "htmlFormat2.l"
 
-#line 657 "lex.yy.c"
+#line 661 "lex.yy.c"
 
 	if ( !(yy_init) )
 		{
@@ -706,13 +710,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 16 )
+				if ( yy_current_state >= 19 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 10 );
+		while ( yy_base[yy_current_state] != 15 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -757,27 +761,35 @@ YY_RULE_SETUP
 imprimir(); endline(); imprimirIndent(); BEGIN 0;
 	YY_BREAK
 case 5:
+*yy_cp = (yy_hold_char); /* undo effects of setting up yytext */
+(yy_c_buf_p) = yy_cp = yy_bp + 1;
+YY_DO_BEFORE_ACTION; /* set up yytext again */
 YY_RULE_SETUP
 #line 9 "htmlFormat2.l"
-indents--; imprimirIndent(); imprimir(); 
+imprimir(); endline(); BEGIN 0;
 	YY_BREAK
 case 6:
-/* rule 6 can match eol */
 YY_RULE_SETUP
 #line 10 "htmlFormat2.l"
-
+indents--; imprimirIndent(); imprimir(); 
 	YY_BREAK
 case 7:
+/* rule 7 can match eol */
 YY_RULE_SETUP
 #line 11 "htmlFormat2.l"
-imprimir(); BEGIN textbefore; 
+
 	YY_BREAK
 case 8:
 YY_RULE_SETUP
 #line 12 "htmlFormat2.l"
+imprimir(); BEGIN textbefore; 
+	YY_BREAK
+case 9:
+YY_RULE_SETUP
+#line 13 "htmlFormat2.l"
 ECHO;
 	YY_BREAK
-#line 781 "lex.yy.c"
+#line 793 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 case YY_STATE_EOF(textbefore):
 case YY_STATE_EOF(indento):
@@ -1071,7 +1083,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 16 )
+			if ( yy_current_state >= 19 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -1099,11 +1111,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 16 )
+		if ( yy_current_state >= 19 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 15);
+	yy_is_jam = (yy_current_state == 18);
 
 	return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1777,7 +1789,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 12 "htmlFormat2.l"
+#line 13 "htmlFormat2.l"
 
 
     void endline(){
